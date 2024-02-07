ff94d89ad1f657f087668e65694e537d8b485502
user <cleosignori@gmail.com>
2024-02-07 06:58:17 -0300

    parametro de valor

--- index.php
+++ index.php
@@ -7,10 +7,34 @@
 </head>
 <body>
   <?php
-  $students = ["a", "b", "c"];
-    foreach ($students as $key ) {
-      echo "Aluno: $key <br>";
-    } 
+  $code = 'curso php';
+
+  echo "<h2>Passagem por valor</h2>";
+  function salary($salaryValue) {
+    // Passagem de parâmetro por valor
+    $salaryValue += 50; // Este valor só é alterado dentro da função, não afeta o valor original da variável;
+    echo "Função salário com aumento $salaryValue <br>";
+  }
+
+  $salaryValue = 8300; // Este valor não vai ser alterado, somente
+
+  salary($salaryValue);
+
+  echo "Salário fora da função: $salaryValue";
+  echo "<hr>";
+
+  function salario_a($num) {
+    $num += 100;
+    echo "Function: Salário com aumento $num <br>";
+    return $num;
+  };
+
+  $salario = 8500.47;
+  $salarioAumento = salario_a($salario);
+
+  echo "Fora da função: $salarioAumento";
+  echo $salario;
+
   ?>
 
 </body>
