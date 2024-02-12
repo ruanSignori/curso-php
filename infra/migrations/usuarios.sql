2d6bb9f7ea7bc382c1951c66f32d892ad69b156d
user <cleosignori@gmail.com>
2024-02-12 17:24:42 -0300

    feat: add 2 columns in usuarios

--- infra/migrations/usuarios.sql
+++ infra/migrations/usuarios.sql
@@ -9,3 +9,9 @@ CREATE TABLE usuarios (
     created DATE,
     modified DATE
 );
+
+ALTER TABLE usuarios
+ADD IF NOT EXISTS sits_usuario_id INT REFERENCES sits_usuario(id);
+
+ALTER TABLE usuarios
+ADD IF NOT EXISTS niveis_acesso_id INT REFERENCES niveis_acesso(id);
